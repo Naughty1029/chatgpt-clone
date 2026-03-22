06574e89c8cc39d726e1ba05908a2dc5050bf380
Naughty1029 <masahiro.watanabe02@gmail.com>
2026-03-22 15:44:45 +0900

    add chats_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "chats/index"
   get "up" => "rails/health#show", as: :rails_health_check
   root "chats#index"
+  resources :chats, only: [ :index, :create ]
 end
